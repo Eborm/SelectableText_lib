e09deceb074acd4bf4948feb64712f0a57017bfb
Eborm <bramwesel@gmail.com>
2025-11-10 16:52:25 +0100

    set up the helperclass

---
+++ helperclass.py
@@ -0,0 +1,25 @@
+import msvcrt
+
+class detection():
+    def detect_up_down() -> int:
+        if msvcrt.kbhit():
+            key = msvcrt.getch().decode('utf-8', errors='ignore')
+            
+            match key:
+                case "P":
+                    return -1
+                case "H":
+                    return 1
+                case "\r":
+                    return 0
+                
+    def detect_left_right() -> int:
+        if msvcrt.kbhit():
+            key = msvcrt.getch().decode('utf-8', errors='ignore')
+            match key:
+                case "M":
+                    return 1
+                case "K":
+                    return -1
+                case "\r":
+                    return 0
