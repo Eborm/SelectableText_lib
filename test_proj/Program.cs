5a6d8d42c81418fa5ed899b3067650befdaa88bd
Eborm <bramwesel@gmail.com>
2026-05-20 10:12:11 +0200

    Updated function names to follow naming convention and changed namespace to showcase_proj as it should have been all the time

--- test_proj/Program.cs
+++ test_proj/Program.cs
@@ -3,7 +3,7 @@ using static System.Net.Mime.MediaTypeNames;
 using SelectableText_lib_namespace.Classes;
 using System.Security.Principal;
 
-namespace test_proj
+namespace showcase_proj
 {
     internal class Program
     {
@@ -67,8 +67,8 @@ namespace test_proj
             SelectableText_lib st = new SelectableText_lib(true, ConsoleColor.Black, ConsoleColor.White); //First variable is used to chose if you want the intro animation
             //Second variable is the background color of the text when it is not selected, third variable is the text color of the text
             //These will be inverted when the text is selected, so the background color will become the text color and the text color will become the background color
-            st.AddText("test", "Test [yes]", myfunction); //Bracketed text is the text that will be highlighted when the user has that row selected
-            st.AddText("test2", "Test 2 [no]", myfunction2); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("test", "Test [yes]", MyFunction); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("test2", "Test 2 [no]", MyFunction2); //Bracketed text is the text that will be highlighted when the user has that row selected
             st.AddText("sometext", "some text"); //This is just text that will be displayed but not selectable as it has no function tied to it
             st.AddText("sometext2", "some text2"); //This is just text that will be displayed but not selectable as it has no function tied to it
             List<string> menu = new List<string> { "test", "empty", "test2", "sometext", "sometext2" };
@@ -84,8 +84,8 @@ namespace test_proj
         {
             SelectableText_lib st = new SelectableText_lib(true, ConsoleColor.Black, ConsoleColor.White); //First variable is used to chose if you want the intro animation
             //Second variable is the background color of the text when it is not selected, third variable is the text color of the text
-            st.AddText("Test [yes]", myfunction); //Bracketed text is the text that will be highlighted when the user has that row selected
-            st.AddText("Test 2 [no]", myfunction2); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("Test [yes]", MyFunction); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("Test 2 [no]", MyFunction2); //Bracketed text is the text that will be highlighted when the user has that row selected
             st.AddText("some text"); //This is just text that will be displayed but not selectable as it has no function tied to it
             st.AddText("some text2"); //This is just text that will be displayed but not selectable as it has no function tied to it
             List<int> menu = new List<int> { 1, 0, 2, 3, 4 };
@@ -101,8 +101,8 @@ namespace test_proj
         {
             SelectableText_lib st = new SelectableText_lib(true, ConsoleColor.Black, ConsoleColor.White); //First variable is used to chose if you want the intro animation
             //Second variable is the background color of the text when it is not selected, third variable is the text color of the text
-            st.AddText("Test [yes] [no]", new List<Action> { myfunction, myfunction2 }); //Bracketed text is the text that will be highlighted when the user has that row selected
-            st.AddText("Test 2 [no] [yes]", new List<Action> { myfunction2, myfunction }); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("Test [yes] [no]", new List<Action> { MyFunction, MyFunction2 }); //Bracketed text is the text that will be highlighted when the user has that row selected
+            st.AddText("Test 2 [no] [yes]", new List<Action> { MyFunction2, MyFunction }); //Bracketed text is the text that will be highlighted when the user has that row selected
             st.AddText("some text"); //This is just text that will be displayed but not selectable as it has no function tied to it
             st.AddText("some text2"); //This is just text that will be displayed but not selectable as it has no function tied to it
             List<int> menu = new List<int> { 1, 0, 2, 3, 4 };
@@ -115,7 +115,7 @@ namespace test_proj
         }
 
         //Showcase function that clears the screen displays some text and waits for the user to press enter before returning to the menu
-        public static void myfunction()
+        public static void MyFunction()
         {
             Console.Clear();
             Console.WriteLine("Test function");
@@ -123,7 +123,7 @@ namespace test_proj
         }
 
         //Showcase function that clears the screen displays some text and waits for the user to press enter before returning to the menu
-        public static void myfunction2()
+        public static void MyFunction2()
         {
             Console.Clear();
             Console.WriteLine("Test function 2");
